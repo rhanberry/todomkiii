981e5d45e53f22546e926a0b1cb5ee802c93d17d
Rick Hanberry <rhanberry@catalystitservices.com>
2015-12-14 15:42:42 -0800

    Added Runner class and package

---
+++ src/main/java/todo/runner/TodoRunner.java
@@ -0,0 +1,15 @@
+package todo.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication	
+public class TodoRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRunner.class);
+	}
+
+}
